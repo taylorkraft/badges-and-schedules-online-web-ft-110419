f39ad6731e03856ee5eb678b265fc8b99300f2a3
Taylor Kraft <taylorkraft16@gmail.com>
2019-11-07 18:23:16 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -3,9 +3,9 @@ def badge_maker(name)
   return "Hello, my name is #{name}."
 end
 
-def batch_badge_creator(names)
+def batch_badge_creator(array_of_names)
   attendees = []
-  names.each do |name|
+  array_of_names.each do |name|
   attendees << "Hello, my name is #{name}."
 end
   return attendees
