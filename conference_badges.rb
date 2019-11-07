dbc70ead708442c88943648dd04959f45ecd4aa3
Taylor Kraft <taylorkraft16@gmail.com>
2019-11-07 18:20:38 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,30 @@
 # Write your code here.
+def badge_maker(name)
+  return "Hello, my name is #{name}."
+end
+
+def batch_badge_creator(array_of_names)
+  attendees = []
+  array_of_names.each do |name|
+  attendees << "Hello, my name is #{name}."
+end
+  return attendees
+end
+
+def assign_rooms(array_of_names)
+  welcome_messages = []
+  counter = 1
+  array_of_names.each_with_index do |name|
+    welcome_messages << "Hello, #{name}! You'll be assigned to room #{counter}!"
+    counter += 1
+  end
+  return welcome_messages
+end 
+
+def printer(attendees)
+  batch_badge_creator(attendees).each do |greeting|
+  end
+  assign_rooms(attendees).each do |assigned_room|
+  end
+  puts printer(attendees)
+end
